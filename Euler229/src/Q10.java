27275f54ffef038b96b06768ddf27933eb584e0d
Harshita248 <46245859+Harshita248@users.noreply.github.com>
2019-01-02 14:29:25 +0530

    Update Q10.java

--- Euler229/src/Q10.java
+++ Euler229/src/Q10.java
@@ -2,16 +2,16 @@
 public class Q10 {
 
 	public static void main(String[] args) {
-		long s=0;
-		for(int i=0;i<2000000;i++)
+		long s=2;
+		for(long i=3;i<2000000;i+=2)
 		     if(prime(i))
 				s+=i;
 		System.out.println("Sum :"+s);
 		}
 			public static boolean prime(long num)
 			{
-			for(int i=2;i<=Math.sqrt(num);i++)
-			   if(num%i==0)
+			for(int j=2;j<=Math.sqrt(num);j++)
+			   if(num%j==0)
 	            return false;
 			
 				return true;
