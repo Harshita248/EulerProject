b106c42064dc3f156059f1b91ee7a63e9b39df90
Harshita248 <46245859+Harshita248@users.noreply.github.com>
2019-01-03 19:56:54 +0530

    Update Q5.java

--- Euler229/src/Q5.java
+++ Euler229/src/Q5.java
@@ -1,40 +1,29 @@
 
 public class Q5 {
-
-	public static void main(String[] args) {
-		System.out.println(smul(20));//to print smallest multiple
-
-	}
-	public static long smul(int num)
+public static long gcd(long a,long b)
 	{
-		long c=fact(num);
-		for(long i=num;i<=c;i=i+num)
-		{
-			if(multiple(i,num))
-				return i;
-		}
-		return -1;
+		if(a%b!=0)
+			return gcd(b,a%b);
+			else
+				return b;
 	}
-public static boolean multiple(long m,int num)
-{
-	for(int i=1;i<num;i++)
+	public static long lcm(long n)
 	{
-		if(m%i!=0)
-			return false;
+		long c=1;
+		for(long i=1;i<=n;i++)
+			c=(c*i)/(gcd(c,i));
+		return c;
 	}
-	return true;
-}
-public static long fact(long num)//for factorial of a number
-{
-	if(num>1)
+	public static void main(String args[])
 	{
-		long x=num*fact(num-1);
-		return x;
-	}
-	else if(num>=0)
-		return 1;
-	else
-		return -1;
+		long n=20;
+		System.out.println("The smallest positive no evenly divisible by all of the number from 1 to 20= "+lcm(n));
 	}
 }
+
+	
+	
+	
+
+
 
